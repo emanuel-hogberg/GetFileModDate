212cb5f2b7782014dbdeba80e7179f6742611b67
Emanuel Högberg <emanuel.hogberg@gmail.com>
2019-05-31 09:06:47 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetFileModDate
+{
+    class Program
+    {
+        /// <summary>
+        /// Usage: GetFileModDate.exe {filename|filepath}
+        /// Outputs: Last modified date (DateTime)
+        /// In SAP Data Services: 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string filepath = string.Empty;
+            try
+            {
+                if (args.Count() == 0)
+                    throw new ArgumentException("Please provide a valid filename or filepath.");
+
+                filepath = args[0];
+
+                if (!File.Exists(filepath))
+                {
+                    try
+                    {
+                        string s = Directory.EnumerateFiles(Path.GetFullPath(filepath))
+                            .Aggregate((a, b) => string.Concat(a, b));
+                        throw new FileNotFoundException($" File '{filepath}' not found, path contents: {s}");
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+
+                    throw new ArgumentException($"File {filepath} does not exist!");
+                }
+
+                Console.Write(File.GetLastWriteTime(filepath));
+            }
+            catch (Exception ex)
+            {
+                Console.Write($"{Environment.UserName} - {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GetFileModDate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GetFileModDate")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ac78fb43-41be-4ce3-9d6f-ca5137e357e2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
